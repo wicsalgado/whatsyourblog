b4b5bbd8912743229d07cb00837e8bace5b6dd7b
wicsalgado <wicrobs@gmail.com>
2026-09-23 19:41:21 -0300

    feature 2 que o professor pediu em sala (prova)

--- posts/forms.py
+++ posts/forms.py
@@ -4,6 +4,8 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    MIN_CONTENT_LENGTH = 50
+
     class Meta:
         model = Post
         fields = [
@@ -19,3 +21,13 @@ class PostForm(forms.ModelForm):
             }),
             'tags': forms.CheckboxSelectMultiple,
         }
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+
+        if content and len(content.strip()) < self.MIN_CONTENT_LENGTH:
+            raise forms.ValidationError(
+                f'O conteúdo do post precisa ter pelo menos {self.MIN_CONTENT_LENGTH} caracteres.'
+            )
+
+        return content
